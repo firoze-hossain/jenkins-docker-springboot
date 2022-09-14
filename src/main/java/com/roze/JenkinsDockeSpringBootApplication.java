fbd02a17373603b191a057e70952c2c21883773d
roze28 <firoze.hossain01@yahoo.com>
2022-09-14 10:38:57 +0600

    first commit

---
+++ src/main/java/com/roze/JenkinsDockeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.roze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockeSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDockeSpringBootApplication.class, args);
+    }
+
+}
